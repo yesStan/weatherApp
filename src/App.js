a4169f80c2581117a85fd41dba62ee2a7d8f4c91
stan yes <stanislav.yeshchenko@dataart.com>
2021-11-02 10:39:55 +0200

    test GET request + location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // function Foo(props) {
+  //   useEffect(() => {
+  //     console.log(props.name);
+  //   }, []); // <-- should error and offer autofix to [props.name]
+  // }
+
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  const savePostionToState = (position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
+
+  const fetchWeather = async () => {
+    try {
+      await window.navigator.geolocation.getCurrentPosition(savePostionToState);
+
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=44f7b107031d8383c8a9b858bb39220d`
+      );
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
